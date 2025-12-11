65bfc9fd00ef539e5cdb29770c960b0deaa2b6ac
StasSweet1206 <stassweet1206@gmail.com>
2025-12-11 23:11:15 +0400

    Добавлен healthcheck.railway.app в ALLOWED_HOSTS

--- sso/settings.py
+++ sso/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-mbs+%ymg#c1if(*mehgn+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,.railway.app,healthcheck.railway.app').split(',')
 
 
 # Application definition
