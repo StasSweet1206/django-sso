6437d4eb1bc6018325a1b4f0e9f895be5fcb0828
StasSweet1206 <stassweet1206@gmail.com>
2025-12-12 01:29:04 +0400

    Add root path for Railway healthcheck

--- sso/urls.py
+++ sso/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.health_check), 
     path('admin/', admin.site.urls),
     path('health/', views.health_check),
 ]
