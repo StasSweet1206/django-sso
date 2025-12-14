b1c42a606e1fcb7494f97235b254e7dfbe410877
StasSweet1206 <stassweet1206@gmail.com>
2025-12-15 00:29:39 +0400

    Add catalog urls

--- sso/urls.py
+++ sso/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.health_check), 
     path('admin/', admin.site.urls),
     path('health/', views.health_check),
+    path('api/catalog/', include('catalog.urls')),
 ]
